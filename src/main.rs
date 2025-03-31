e5a12476d25259fc2b190ac34cf022390b6b3be7
soya k <sy.kwsk@gmail.com>
2025-03-31 15:10:09 +0900

    更新

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     //let tester = Tetri::new(0, 3, 21, Mino::T); 
     //println!("tester : {}, {}, {}, {:?}" , tester.rot(), tester.x(), tester.y(), tester.id());
-    let mut my_rng = rand::thread_rng();
+    let mut my_rng = rand::rng();
     let mut test_obj = TetriObj::new();
     test_obj.init(&mut my_rng, 1);
 
